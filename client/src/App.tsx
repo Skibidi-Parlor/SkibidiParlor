fafa63951b7bb9540acdaa8977942668bcf42837
ckchung24 <143671780+TheBigTig24@users.noreply.github.com>
2025-04-09 23:16:14 -0700

    feature: login page CSS (#4)
    * really simple mobile version of login page
    
    * basic login page for mobile
    
    * testing
    
    * refuck
    
    * ok i think i fixed my git ignore those other things
    
    * merged from main
    
    ---------
    
    Co-authored-by: DanielPasion <dnpaxion@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home";
+import Login from "./pages/login";
 
 import SliceSweeper from "./pages/games/slice_sweeper";
 import { useQuery } from "@tanstack/react-query";
@@ -15,6 +16,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
 
       <Route path="games">
         <Route path="SliceSweeper" element={<SliceSweeper />} />
