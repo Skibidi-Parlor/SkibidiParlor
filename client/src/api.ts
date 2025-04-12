dddb35204a8b18d505c271b581888b0cb7677bc1
DanielPasion <90020974+DanielPasion@users.noreply.github.com>
2025-04-12 06:02:17 -0700

    prod urls (#13)

--- client/src/api.ts
+++ client/src/api.ts
@@ -7,16 +7,21 @@ import {
 import type { AppRouter } from "../server/index.ts";
 import { transformer } from "../shared/src/transformer.ts";
 
+const URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:4000"
+    : "https://skibidiparlortrpc.onrender.com";
+
 export const trpc = createTRPCClient<AppRouter>({
   links: [
     splitLink({
       condition: (op) => op.type === "subscription",
       true: httpSubscriptionLink({
-        url: "http://localhost:3000",
+        url: URL,
         transformer,
       }),
       false: httpBatchStreamLink({
-        url: "http://localhost:3000",
+        url: URL,
         transformer,
       }),
     }),
