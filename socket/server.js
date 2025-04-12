808752072fbeca0edd9a8933d0b26c749fcd6eff
DanielPasion <dnpaxion@gmail.com>
2025-04-12 06:08:54 -0700

    CORS

--- socket/server.js
+++ socket/server.js
@@ -3,9 +3,7 @@ const io = require("socket.io")(4000, {
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://clipr.vercel.app",
-      "https://clipr-danielpasions-projects.vercel.app",
-      "https://clipr-git-main-danielpasions-projects.vercel.app",
+      "https://skibidi-parlor.vercel.app",
     ],
   },
 });
