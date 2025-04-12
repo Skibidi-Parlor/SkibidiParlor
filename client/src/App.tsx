d0b7193354934554a10182b0af87208e012cec7c
chillwafflez <ju5t1n.nguy3n284@gmail.com>
2025-04-12 03:08:28 -0700

    added basic buttons to move to create account and login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,9 +9,7 @@ import TriviaPlayer from "./pages/trivia/player";
 import TriviaScreen from "./pages/trivia/screen";
 import TriviaAdmin from "./pages/trivia/admin";
 
-import { useQuery } from "@tanstack/react-query";
 import { trpc } from "./api";
-import { TRPCClientError } from '@trpc/client';
 
 function App() {
   // testing user.byID
@@ -33,48 +31,6 @@ function App() {
   // });
   // console.log(data);
 
-  // testing user.create
-  const skibidi = async () => {
-    try {
-      const newUser = await trpc.user.create.mutate({
-        username: "Mark S",
-        nickname: "Outie Mark",
-        email: "ihatemylife@gmail.com",
-        password: "ilovemywife",
-        pfp_path: "https://random.image/img3.jpg"
-      });
-      console.log("created user: ", newUser);
-    } catch (error) {
-      console.log("unable to create new user: ", error)
-    }
-  }
-  // skibidi();
-
-  // test user.login
-  const yuhh = async() => {
-    try {
-      const userID = await trpc.auth.login.mutate({
-        email: "ihatemylife@gmail.com",
-        password: "ilovemywife"
-      });
-      console.log("fetched user ID after logging in: ", userID);
-    } catch (error) {
-      console.log("couldnt log in");
-
-      if (error instanceof TRPCClientError) {
-        if (error.data?.code === 'NOT_FOUND') {
-          console.log("no user with that email found");
-        } else if (error.data?.code === 'UNAUTHORIZED') {
-          console.log("incorrect password");
-        } else {
-          console.log("other tRPC server error: " + error.message);
-        }
-      } else {
-        console.log("error:", error);
-      }
-    }
-  }
-  // yuhh();
 
   return (
     <>
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
 const home = () => {
-  return <h1 className="text-3xl font-bold underline">Home</h1>;
+
+  return (
+    <div className="bg-[#B9C0DA] min-w-screen min-h-screen flex flex-col items-center ">
+      <div className="flex flex-col justify-center w-full p-5 mt-[2rem] items-center rounded-lg gap-y-5">
+        <img ></img>
+        <div className="bg-[#FE7F2D] hover:bg-[#e35a01] text-white font-bold py-2 px-4 rounded-lg text-[1.5rem] cursor-pointer">
+          <Link to="./login" className="text-white px-5 py-5">Login</Link>
+        </div>
+
+        <div className="bg-[#FE7F2D] hover:bg-[#e35a01] text-white font-bold py-2 px-4 rounded-lg text-[1.5rem] cursor-pointer">
+          <Link to="./createAcc" className="text-white px-5 py-5">Create Account</Link>
+        </div>
+      </div>
+
+    </div>
+
+  );
 };
 
 export default home;
