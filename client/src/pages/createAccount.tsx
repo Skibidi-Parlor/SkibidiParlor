4448f91af1c519b03f11727f276c7fa7233e1183
chillwafflez <ju5t1n.nguy3n284@gmail.com>
2025-04-12 00:32:53 -0700

    slight refactor of create account page to make it more responsive

--- client/src/pages/createAccount.tsx
+++ client/src/pages/createAccount.tsx
@@ -1,29 +1,54 @@
 const CreateAccount = () => {
 
+    const createAccount = async (event: React.FormEvent<HTMLFormElement>) => { 
+
+        console.log("skibidi");
+
+    }
+
     return(
     <div className="bg-[#B9C0DA] min-w-screen min-h-screen flex flex-col items-center ">
-        <h1 className="text-7xl font-bold text-center mt-[2rem]">
-            Skibidi Parlor
-        </h1>
-        <div className="h-[60vh] w-[90vw] lg:h-[60vh] lg:w-[35vw] bg-white rounded-md p-[1rem] flex flex-col mt-auto mb-auto p-8">
-            <p className="text-[1.7rem] font-bold mt-[1rem]">username</p>
-            <input className="border-b-1 focus:outline-none focus:border-b-2 mt-[1rem]"></input>
+        <div className="flex flex-col justify-center w-[90vw] lg:w-[35vw] p-5 mt-[2rem] items-center bg-white rounded-lg">
 
-            <p className="text-[1.7rem] mt-[2rem] font-bold pt-[.6rem]">password</p>
-            <input
-            type="password"
-            className="border-b-1 focus:outline-none focus:border-b-2 mt-[1rem]"
-            ></input>
+            <form onSubmit={createAccount} className="w-full space-y-5">
 
-            <p className="text-[1.7rem] mt-[2rem] font-bold pt-[.6rem]">re-enter password</p>
-            <input
-            type="password"
-            className="border-b-1 focus:outline-none focus:border-b-2 mt-[1rem]"
-            ></input>
+                <h1 className="text-4xl font-bold text-center mt-[1.5rem]">
+                    Create Account
+                </h1>
 
-            <button className="w-[50%] h-[12.5%] bg-[#FE7F2D] hover:bg-[#e35a01] text-white rounded-lg self-center mt-auto text-[2.3rem] font-semibold">
-            Login
-            </button>
+                <div>
+                    <p className="text-[1.5rem] font-bold mt-[2rem]">email</p>
+                    <input className="w-full border-b-1 focus:outline-none mt-[0.5rem]" type="email"></input>
+                </div>
+
+                <div>
+                    <p className="text-[1.5rem] font-bold mt-[1rem]">password</p>
+                    <input className="w-full border-b-1 focus:outline-none mt-[0.5rem]" type="password"></input>
+                </div>
+
+                <div>
+                    <p className="text-[1.5rem] font-bold mt-[1rem]">re-enter password</p>
+                    <input className="w-full border-b-1 focus:outline-none mt-[0.5rem]" type="password"></input>
+                </div>
+
+                <div>
+                    <p className="text-[1.5rem] font-bold mt-[1rem]">username</p>
+                    <input className="w-full border-b-1 focus:outline-none mt-[0.5rem]" ></input>
+                </div>
+
+                <div>
+                    <p className="text-[1.5rem] font-bold mt-[1rem]">nickname</p>
+                    <input className="w-full border-b-1 focus:outline-none mt-[0.5rem]"></input>
+                </div>
+
+                <div className="flex justify-center mt-[2rem]">
+                    <button 
+                        type="submit"
+                        className="bg-[#FE7F2D] hover:bg-[#e35a01] text-white font-bold py-2 px-4 rounded-lg text-[1.5rem] cursor-pointer">Login
+                    </button>
+                </div>
+            </form>
+
         </div>
     </div>);
 };
