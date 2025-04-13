821d76657d5a0cc3f18b5ece07e2c9ea442f99ed
DanielPasion <dnpaxion@gmail.com>
2025-04-13 06:08:55 -0700

    home page wip

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
 
 const home = () => {
-
   return (
-    <div className="bg-[#B9C0DA] min-w-screen min-h-screen flex flex-col items-center ">
+    <div className="bg-linear-to-b from-[#7134DD] to-[#AF9CCF] min-w-screen min-h-screen flex flex-col items-center ">
       <div className="flex flex-col justify-center w-full p-5 mt-[2rem] items-center rounded-lg gap-y-5">
-        <img ></img>
-        <div className="bg-[#FE7F2D] hover:bg-[#e35a01] text-white font-bold py-2 px-4 rounded-lg text-[1.5rem] cursor-pointer">
-          <Link to="./login" className="text-white px-5 py-5">Login</Link>
+        <img></img>
+        <div className="bg-white hover:bg-[#e35a01] font-bold py-2 px-4 rounded-lg text-[1.5rem] cursor-pointer">
+          <Link to="./login" className="text-[#3D1C77] px-5 py-5">
+            Login
+          </Link>
         </div>
 
-        <div className="bg-[#FE7F2D] hover:bg-[#e35a01] text-white font-bold py-2 px-4 rounded-lg text-[1.5rem] cursor-pointer">
-          <Link to="./createAcc" className="text-white px-5 py-5">Create Account</Link>
+        <div className="bg-white hover:bg-[#e35a01] font-bold py-2 px-4 rounded-lg text-[1.5rem] cursor-pointer">
+          <Link to="./createAcc" className="text-[#3D1C77] px-5 py-5">
+            Create Account
+          </Link>
         </div>
       </div>
-
     </div>
-
   );
 };
 
